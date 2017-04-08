dd8dc2b9c8fdcdd1b935fa413a6ba1a3223263b8
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-04-09 02:39:23 +0300

    Lesson1 -> mamapapa. Print mama and papa

---
+++ semestr1/semestr1/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  semestr1
+//
+//  Created by Aleksey Zuiev on 09/04/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+#include <stdio.h>
+#include "mamapapa.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    for (int i = 0; i<15; i++) {
+        printMamaPapa(i);
+    }
+    return 0;
+}
---
+++ semestr1/semestr1/mamapapa.h
@@ -0,0 +1,16 @@
+//
+//  mamapapa.h
+//  semestr1
+//
+//  Created by Aleksey Zuiev on 09/04/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+#ifndef mamapapa_h
+#define mamapapa_h
+
+#include <stdio.h>
+
+void printMamaPapa(int value);
+
+#endif /* mamapapa_h */
