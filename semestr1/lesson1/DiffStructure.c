97df876822ba10482fca20a0946e99ce0b28961f
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-04-12 01:15:42 +0300

    Order of structure fields -> sorting added

--- semestr1/lesson1/DiffStructure.c
+++ semestr1/lesson1/DiffStructure.c
@@ -9,23 +9,7 @@
 #include "DiffStructure.h"
 #include <stddef.h>
 
-const int fieldsCount = 3;
-//struct AZDiffStructure {
-//    char * char1;
-//    double double1;
-//    short short1;
-//    short short2;
-//    short short3;
-//    long long llong1;
-//    int int1;
-//    float float1;
-//    bool bool1;
-//    bool bool2;
-//    bool bool3;
-//    bool bool4;
-//    bool bool5;
-//    bool bool6;
-//};
+const int fieldsCount = 14;
 
 struct AZFieldsOrder{
     short offset;
@@ -38,17 +22,58 @@ typedef  struct AZDiffStructure AZStruct;
     array[i].name = #filed;              \
     array[i].offset = offsetof(type , filed)
 
+void AZSwapOffset(struct AZFieldsOrder *array, index1,index2){
+    int i = array[index1].offset;
+    char * s = array[index1].name;
+    array[index1].offset = array[index2].offset;
+    array[index1].name = array[index2].name;
+    array[index2].offset = i;
+    array[index2].name = s;
+}
+
+void AZSort (struct AZFieldsOrder *array) {
+    if (fieldsCount < 2) return;
+    int minI, maxI;
+    for (int i=0;i<fieldsCount/2;i++){
+        minI = i;
+        maxI = i;
+        for(int j=i+1;j<fieldsCount-i;j++){
+            if (array[minI].offset>array[j].offset)
+                minI = j;
+            if (array[maxI].offset<array[j].offset)
+                maxI = j;
+        }
+        if (minI!=i) AZSwapOffset(array,i,minI);
+        if (maxI!=fieldsCount-i-1) AZSwapOffset(array,i,minI);
+        
+    }
+}
+
+
 void AZGetFieldPosition(){
     struct AZFieldsOrder order[fieldsCount];
 
     AZGenStruct(order, 0, char1, AZStruct);
     AZGenStruct(order, 1, short1, AZStruct);
     AZGenStruct(order, 2, double1, AZStruct);
+    AZGenStruct(order, 3, short2, AZStruct);
+    AZGenStruct(order, 4, short3, AZStruct);
+    AZGenStruct(order, 5, llong1, AZStruct);
+    AZGenStruct(order, 6, int1, AZStruct);
+    AZGenStruct(order, 7, float1, AZStruct);
+    AZGenStruct(order, 8, bool1, AZStruct);
+    AZGenStruct(order, 9, bool2, AZStruct);
+    AZGenStruct(order, 10, bool3, AZStruct);
+    AZGenStruct(order, 11, bool4, AZStruct);
+    AZGenStruct(order, 12, bool5, AZStruct);
+    AZGenStruct(order, 13, bool6, AZStruct);
     
+    AZSort(order);
     for(int i=0;i<fieldsCount;i++){
-        printf("%7s has offset %i\n", order[i].name,order[i].offset);
+        printf("%2i -> %7s has offset %i\n",i, order[i].name,order[i].offset);
     }	
- 
-    
-    
+   
 }
+
+
+
