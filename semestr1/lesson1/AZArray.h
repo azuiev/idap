eec2fc834eb06b2691a6d81d00b5df9ff8e89d1c
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-04-12 22:28:41 +0300

    AZArray ->  create array

---
+++ semestr1/lesson1/AZArray.h
@@ -0,0 +1,18 @@
+//
+//  AZArray.h
+//  lesson1
+//
+//  Created by Aleksey Zuiev on 12/04/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+#ifndef AZArray_h
+#define AZArray_h
+
+#include <stdio.h>
+
+#endif /* AZArray_h */
+
+typedef struct AZArray AZArray;
+
+AZArray *AZArrayCreate();
